d4a6a052bbab37e4feb89ae5a7293e08bc554cfc
coderAndrew37 <omolloandrew37@gmail.com>
2024-12-04 10:26:18 +0300

    Implementing whatsap shake animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,17 @@ module.exports = {
       borderRadius: {
         xl: "1.25rem",
       },
+      keyframes: {
+        shake: {
+          "0%, 100%": { transform: "translateX(0)" },
+          "25%": { transform: "translateX(-5px)" },
+          "50%": { transform: "translateX(5px)" },
+          "75%": { transform: "translateX(-5px)" },
+        },
+      },
+      animation: {
+        shake: "shake 1.5s ease-in-out infinite",
+      },
     },
   },
   plugins: [],
