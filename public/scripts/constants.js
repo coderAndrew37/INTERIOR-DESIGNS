79022130b2af0ceb7d8cbd4d5033769d7c53cf23
coderAndrew37 <omolloandrew37@gmail.com>
2024-12-25 10:38:39 +0300

    .

--- public/scripts/constants.js
+++ public/scripts/constants.js
@@ -1,4 +1,4 @@
 export const baseUrl =
   window.location.hostname === "localhost"
     ? "http://localhost:8000" // Local environment
-    : "hhttps://interiors-by-tiffi.onrender.com"; // Production environment
+    : "https://interiors-by-tiffi.onrender.com"; // Production environment
