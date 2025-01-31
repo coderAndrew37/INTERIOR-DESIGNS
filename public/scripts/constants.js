b85ab85a65569c738057fdd5f607297ad0c3c6c5
= <=>
2025-01-31 21:11:18 +0300

    fixing the base url

--- public/scripts/constants.js
+++ public/scripts/constants.js
@@ -1,4 +1,4 @@
 export const baseUrl =
   window.location.hostname === "localhost"
     ? "http://localhost:8000" // Local environment
-    : "https://interior-designs-gvst.onrender.com"; // Production environment
+    : "https://interiors-by-tiffi.vercel.app"; // Production environment
